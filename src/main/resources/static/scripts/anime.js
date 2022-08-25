5bf81ddfdd101aa8fff6c411469950970a8a9c58
Alamega <vladshuman@gmail.com>
2022-08-25 21:13:49 +0300

    Do you know de way?

--- src/main/resources/static/scripts/anime.js
+++ src/main/resources/static/scripts/anime.js
@@ -1,8 +1,12 @@
 function anime() {
-    const styleSheet = document.createElement("style");
-    styleSheet.innerText = `
-    * {
-        animation: rotate 5s linear infinite;
+    const anime_stylesheet = document.createElement("style");
+    anime_stylesheet.innerText = `
+      body { 
+        overflow-x: hidden;
+        overflow-y: hidden;
+      }
+      .container * {
+        animation: rotate 2s linear infinite;
       }
       @keyframes rotate {
         from {
@@ -12,6 +16,66 @@ function anime() {
           transform: rotate(360deg);
         }
       }
+      @keyframes knock {
+        from {
+          transform: rotate(-25deg);
+        }
+        50% { 
+          transform: rotate(25deg);
+        }
+        to {
+          transform: rotate(-25deg);
+        }
+      }
+      @keyframes move1 {
+        from {
+          margin-left: 200px;
+        }
+        50% { 
+          margin-left: -200px;
+        }
+        to {
+          margin-left: 200px;
+        }
+      }
+      @keyframes move2 {
+        from {
+          margin-left: -200px;
+        }
+        50% { 
+          margin-left: 200px;
+        }
+        to {
+          margin-left: -200px;
+        }
+      }
     `;
-    document.head.appendChild(styleSheet);
+    document.head.appendChild(anime_stylesheet);
+
+    for (let i = 0; i < 1; i++) {
+        const idol = document.createElement("img");
+        idol.src = "/images/idol.png";
+        idol.height = 128;
+        idol.width = 128;
+        idol.style.position = "absolute";
+        idol.style.zIndex = "1000";
+        idol.id = "idol";
+        idol.style.top = Math.random() * window.innerHeight - 64 + "px";
+        idol.style.left = Math.random() * window.innerWidth - 64 + "px";
+        if (i%2===0) {
+            idol.style.animation = "knock 0.4s linear infinite, move1 4s linear infinite";
+        } else {
+            idol.style.animation = "knock 0.4s linear infinite, move2 4s linear infinite";
+        }
+        idol.addEventListener("click", () => {
+            idol.remove();
+        })
+        document.getElementsByClassName("container").item(0).appendChild(idol);
+    }
+
+    setInterval(() => {
+        if (!document.getElementById("idol")) {
+            document.getElementsByTagName("style")[1].remove();
+        }
+    }, 1000)
 }
