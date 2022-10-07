7db9e5b9dbf226271dda93425f08e346a4231dfa
Alamega <vladshuman@gmail.com>
2022-10-07 13:05:48 +0300

    post \n -> <br/>

--- src/main/java/com/alamega/alamegaspringapp/controllers/MainController.java
+++ src/main/java/com/alamega/alamegaspringapp/controllers/MainController.java
@@ -2,19 +2,10 @@ package com.alamega.alamegaspringapp.controllers;
 
 import com.alamega.alamegaspringapp.user.User;
 import com.alamega.alamegaspringapp.user.UserRepository;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.cache.ICacheManager;
-import org.thymeleaf.cache.StandardCacheManager;
-import org.thymeleaf.templateresolver.DefaultTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
-
-import java.util.Collection;
 
 @Controller
 public class MainController {
