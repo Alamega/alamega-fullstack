b4921bb3897ddd79bd87c1894f26d21d1e9083e7
alamega <vladshuman@gmial.com>
2022-10-17 13:08:12 +0300

    chat now can save last 10 message yes my engleash ites very harosh

--- src/main/java/com/alamega/alamegaspringapp/chat/ChatWebSocketHandler.java
+++ src/main/java/com/alamega/alamegaspringapp/chat/ChatWebSocketHandler.java
@@ -5,20 +5,28 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
 
 public class ChatWebSocketHandler extends TextWebSocketHandler {
     private final List<WebSocketSession> webSocketSessions = new ArrayList<>();
+    private final Queue<String> textMessages = new LinkedList<>();
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) {
+    public void afterConnectionEstablished(WebSocketSession session) throws IOException {
+        for (String textMessage : textMessages) {
+            session.sendMessage(new TextMessage(textMessage));
+        }
         webSocketSessions.add(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         if (!message.getPayload().equals("ping")) {
+            textMessages.offer(message.getPayload());
+            if (textMessages.size() >= 10) {
+                textMessages.poll();
+            }
             for (WebSocketSession webSocketSession : webSocketSessions) {
                 webSocketSession.sendMessage(message);
             }
