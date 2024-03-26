8b1824214a904d2434934a24ac5da787b6a79e00
Alamega <vladshuman@gmail.com>
2024-03-26 18:22:08 +0300

    опять

--- backend/src/main/java/com/alamega/backend/config/secutity/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/alamega/backend/config/secutity/JwtAuthenticationFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,7 +17,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
-@DependsOn("jwtService")
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
